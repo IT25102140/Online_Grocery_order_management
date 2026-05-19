a165c34bd818ced8bfaa0f6fde6427900b3d3dc0
IT25102378 <IT25102378@my.sliit.lk>
2026-05-19 12:21:57 +0530

    Update with comments

--- Backend/src/main/java/com/onlinegrocery/backend/service/PaymentService.java
+++ Backend/src/main/java/com/onlinegrocery/backend/service/PaymentService.java
@@ -7,7 +7,7 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.stream.Collectors;
-
+//Service
 @Service
 public class PaymentService {
     private static final String FILE_PATH = "payments.json";
